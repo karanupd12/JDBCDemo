b48d2ae1493633f7cf550882e277a934faf3a672
karan Upadhyay <karanupd12@gmail.com>
2025-04-07 12:04:32 +0530

    CRUD started

--- src/DemoJDBC.java
+++ src/DemoJDBC.java
@@ -9,22 +9,41 @@ Steps :-
 7. Close the connection/resources
  */
 
-//step1
+//step 1
 import java.sql.*;
 
 public class DemoJDBC {
     public static void main(String[] args) throws Exception {
 
-        //step2 - optional in JDBC4 +
+        //step 2 - optional in JDBC4 +
         //Class.forName("org.postgresql.Driver");
 
-        //step3
+        //step 3
         String url = System.getenv("db_url");
         String user = System.getenv("db_user");
         String password = System.getenv("db_password");
-
         Connection con = DriverManager.getConnection(url, user, password);
-
         System.out.println("Connection Established");
+
+        //step 4
+        Statement st = con.createStatement();
+
+        //step 5
+        String sqlQuery = "select * from student";
+        ResultSet rs = st.executeQuery(sqlQuery);
+
+        //step 6
+        while(rs.next()){
+            System.out.println(
+                    rs.getInt(1) + " " +
+                    rs.getString(2)+ " " +
+                    rs.getInt(3)
+            );
+        }
+
+        //step 7
+        con.close();
+        System.out.println("Connection Closed");
+
     }
 }
